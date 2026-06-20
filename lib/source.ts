7b0bf291ff24cc3cb1a10db666cb45cf488c5381
chonk-lain <lain@chonkie.ai>
2026-06-20 07:07:15 +0100

    link with other docs

--- lib/source.ts
+++ lib/source.ts
@@ -1,7 +1,13 @@
-import { docs } from "@/.source";
 import { loader } from "fumadocs-core/source";
+import { docs } from "collections/server";
+import { docs as chonkiejsDocs } from "collections/chonkiejs/server";
 
-export const source = loader({
-  baseUrl: "/docs",
-  source: docs.toFumadocsSource(),
-});
+export const source = loader(
+  {
+    chonkie: docs.toFumadocsSource({ baseDir: "chonkie" }),
+    chonkiejs: chonkiejsDocs.toFumadocsSource({ baseDir: "chonkiejs" }),
+  },
+  {
+    baseUrl: "/docs",
+  },
+);
--- source.config.ts
+++ source.config.ts
@@ -1,7 +1,15 @@
 import { defineDocs, defineConfig } from "fumadocs-mdx/config";
+import * as chonkiejsConfig from "./chonkiejs/source.config";
 
 export const docs = defineDocs({
   dir: "content/docs",
 });
 
-export default defineConfig();
+export default defineConfig({
+  workspaces: {
+    chonkiejs: {
+      dir: "./chonkiejs",
+      config: chonkiejsConfig,
+    },
+  },
+});
